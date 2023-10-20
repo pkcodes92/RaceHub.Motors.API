cdb3cf10878980badcfd071dd1e9e9c5439c332b
Pranav Krishnamurthy <pranav.krishnamurthy@emovis.us>
2023-10-19 22:18:17 -0400

    Adding in the necessary request models to add the manufacturer and vehicle

---
+++ RaceHub.Motors.API.DTO/Request/AddManufacturerRequest.cs
@@ -0,0 +1,52 @@
+﻿// <copyright file="AddManufacturerRequest.cs" company="RaceHub">
+// Copyright (c) RaceHub. All rights reserved.
+// </copyright>
+
+#nullable disable
+
+namespace RaceHub.Motors.API.DTO.Request
+{
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// This class represents the request when adding a new manufacturer.
+    /// </summary>
+    public class AddManufacturerRequest
+    {
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the country.
+        /// </summary>
+        [JsonProperty("country")]
+        public string Country { get; set; }
+
+        /// <summary>
+        /// Gets or sets the country code.
+        /// </summary>
+        [JsonProperty("countryCode")]
+        public string CountryCode { get; set; }
+
+        /// <summary>
+        /// Gets or sets the region.
+        /// </summary>
+        [JsonProperty("region")]
+        public string Region { get; set; }
+
+        /// <summary>
+        /// Gets or sets the flag.
+        /// </summary>
+        [JsonProperty("flag")]
+        public string Flag { get; set; }
+
+        /// <summary>
+        /// Gets or sets the logo.
+        /// </summary>
+        [JsonProperty("logo")]
+        public string Logo { get; set; }
+    }
+}
---
+++ RaceHub.Motors.API.DTO/Request/AddVehicleRequest.cs
@@ -0,0 +1,46 @@
+﻿// <copyright file="AddVehicleRequest.cs" company="RaceHub">
+// Copyright (c) RaceHub. All rights reserved.
+// </copyright>
+
+#nullable disable
+
+namespace RaceHub.Motors.API.DTO.Request
+{
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// This class represents the request when adding a new vehicle.
+    /// </summary>
+    public class AddVehicleRequest
+    {
+        /// <summary>
+        /// Gets or sets the manufacturer name.
+        /// </summary>
+        [JsonProperty("manufacturer")]
+        public string Manufacturer { get; set; }
+
+        /// <summary>
+        /// Gets or sets the vehicle name.
+        /// </summary>
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the vehicle type.
+        /// </summary>
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        /// <summary>
+        /// Gets or sets the vehicle year.
+        /// </summary>
+        [JsonProperty("year")]
+        public int Year { get; set; }
+
+        /// <summary>
+        /// Gets or sets the image of the vehicle.
+        /// </summary>
+        [JsonProperty("image")]
+        public string Image { get; set; }
+    }
+}
