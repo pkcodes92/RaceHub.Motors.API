c5d577ee53ca6d0c188aaafc6ad29297149de2b0
Pranav Krishnamurthy <pranav.krishnamurthy@emovis.us>
2023-10-21 10:21:16 -0400

    Making sure to have the Newtonsoft.Json library being used correctly

--- RaceHub.Motors.API.DTO/Models/Vehicle.cs
+++ RaceHub.Motors.API.DTO/Models/Vehicle.cs
@@ -16,30 +16,36 @@ public class Vehicle
     /// <summary>
     /// Gets or sets the primary key.
     /// </summary>
+    [JsonProperty("id")]
     public int Id { get; set; }
 
     /// <summary>
     /// Gets or sets the manufacturer name.
     /// </summary>
+    [JsonProperty("manufacturer")]
     public string Manufacturer { get; set; }
 
     /// <summary>
     /// Gets or sets the name of the vehicle.
     /// </summary>
+    [JsonProperty("name")]
     public string Name { get; set; }
 
     /// <summary>
     /// Gets or sets the vehicle type code.
     /// </summary>
+    [JsonProperty("typeCode")]
     public string TypeCode { get; set; }
 
     /// <summary>
     /// Gets or sets the image of the vehicle.
     /// </summary>
+    [JsonProperty("image")]
     public string Image { get; set; }
 
     /// <summary>
     /// Gets or sets the year of the vehicle.
     /// </summary>
+    [JsonProperty("year")]
     public int Year { get; set; }
 }
