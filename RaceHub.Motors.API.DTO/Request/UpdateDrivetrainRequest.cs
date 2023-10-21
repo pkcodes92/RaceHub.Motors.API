302784e73c4c2a32400071c3094183f71db37460
Pranav Krishnamurthy <pranav.krishnamurthy@emovis.us>
2023-10-20 20:24:46 -0400

    Having the necessary request models for the Update operation

---
+++ RaceHub.Motors.API.DTO/Request/UpdateDrivetrainRequest.cs
@@ -0,0 +1,34 @@
+﻿// <copyright file="UpdateDrivetrainRequest.cs" company="RaceHub">
+// Copyright (c) RaceHub. All rights reserved.
+// </copyright>
+
+#nullable disable
+
+namespace RaceHub.Motors.API.DTO.Request
+{
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// This class represents the request when updating the drivetrain.
+    /// </summary>
+    public class UpdateDrivetrainRequest
+    {
+        /// <summary>
+        /// Gets or sets the primary key.
+        /// </summary>
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the code.
+        /// </summary>
+        [JsonProperty("code")]
+        public string Code { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description.
+        /// </summary>
+        [JsonProperty("description")]
+        public string Description { get; set; }
+    }
+}
---
+++ RaceHub.Motors.API.DTO/Request/UpdateVehicleTypeRequest.cs
@@ -0,0 +1,34 @@
+﻿// <copyright file="UpdateVehicleTypeRequest.cs" company="RaceHub">
+// Copyright (c) RaceHub. All rights reserved.
+// </copyright>
+
+#nullable disable
+
+namespace RaceHub.Motors.API.DTO.Request
+{
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// This class will be used to update a vehicle type in the database.
+    /// </summary>
+    public class UpdateVehicleTypeRequest
+    {
+        /// <summary>
+        /// Gets or sets the primary key.
+        /// </summary>
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the code.
+        /// </summary>
+        [JsonProperty("code")]
+        public string Code { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description.
+        /// </summary>
+        [JsonProperty("description")]
+        public string Description { get; set; }
+    }
+}
