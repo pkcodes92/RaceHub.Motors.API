f7d4eb3a7046806b7316dcce91675e6a4d07f873
pkcodes92 <pranav.krishnamurthy@emovis.com>
2024-02-05 21:10:30 -0500

    Having things updated with the user repository and the interface

--- RaceHub.Motors.API.DAL/Entity/User.cs
+++ RaceHub.Motors.API.DAL/Entity/User.cs
@@ -11,26 +11,59 @@ namespace RaceHub.Motors.API.DAL.Entity
     /// </summary>
     public class User
     {
+        /// <summary>
+        /// Gets or sets the ID.
+        /// </summary>
         public int Id { get; set; }
 
+        /// <summary>
+        /// Gets or sets the first name.
+        /// </summary>
         public string FirstName { get; set; }
 
+        /// <summary>
+        /// Gets or sets the last name.
+        /// </summary>
         public string LastName { get; set; }
 
+        /// <summary>
+        /// Gets or sets the type ID.
+        /// </summary>
         public int TypeId { get; set; }
 
+        /// <summary>
+        /// Gets or sets the date/time that the user is created.
+        /// </summary>
         public DateTime Created { get; set; }
 
+        /// <summary>
+        /// Gets or sets who has created the user.
+        /// </summary>
         public string CreatedBy { get; set; }
 
+        /// <summary>
+        /// Gets or sets the date/time that the user was updated.
+        /// </summary>
         public DateTime LastUpd { get; set; }
 
+        /// <summary>
+        /// Gets or sets who has updated the user.
+        /// </summary>
         public string LastUpdBy { get; set; }
 
+        /// <summary>
+        /// Gets or sets the app which updated the user.
+        /// </summary>
         public string LastUpdApp { get; set; }
 
+        /// <summary>
+        /// Gets or sets the email address.
+        /// </summary>
         public string EmailAddress { get; set; }
 
+        /// <summary>
+        /// Gets or sets the password.
+        /// </summary>
         public string Password { get; set; }
     }
 }
---
+++ RaceHub.Motors.API.DAL/Repository/Interfaces/IUserRepository.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="IUserRepository.cs" company="RaceHub">
+// Copyright (c) RaceHub. All rights reserved.
+// </copyright>
+
+namespace RaceHub.Motors.API.DAL.Repository.Interfaces
+{
+    using RaceHub.Motors.API.DAL.Entity;
+
+    /// <summary>
+    /// This interface defines the methods interact with the User entity in the database.
+    /// </summary>
+    public interface IUserRepository
+    {
+        Task<User> AddUserAsync(User user);
+
+        Task<User> UpdateUserAsync(User user);
+
+        Task<User> GetUserByIdAsync(int id);
+
+        Task<User> GetUserByEmailAsync(string email);
+
+        Task<User> GetUserByEmailAndPasswordAsync(string email, string password);
+
+        Task<bool> RemoveUserAsync(int id);
+    }
+}
